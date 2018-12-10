963fb1a423bdfe89bef0b7bdd05b0dd2464f3561
Fernando Oliveira <Fernando.Oliveira@enernoc.com>
2018-12-10 08:16:39 -0200

    Changing box shadows

--- src/components/home/how-it-works.jsx
+++ src/components/home/how-it-works.jsx
@@ -5,20 +5,20 @@ class HowItWorks extends Component {
   render() {
     return (
       <div className="container">
-        <div class="about-services features-2">
-          <div class="row">
-            <div class="col-md-8 ml-auto mr-auto text-center">
-              <h2 class="title">Veja como é simples receber doações</h2>
+        <div className="about-services features-2">
+          <div className="row">
+            <div className="col-md-8 ml-auto mr-auto text-center">
+              <h2 className="title">Veja como é simples receber doações</h2>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-4">
-              <div class="info info-horizontal">
-                <div class="icon icon-rose">
-                  <i class="material-icons">card_giftcard</i>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="info info-horizontal">
+                <div className="icon icon-rose">
+                  <i className="material-icons">card_giftcard</i>
                 </div>
-                <div class="description">
-                  <h4 class="info-title">1. Escolha uma doação</h4>
+                <div className="description">
+                  <h4 className="info-title">1. Escolha uma doação</h4>
                   <p>
                     Crie uma conta. Acesse a sua plataforma preferida de doações
                     e procure por doações.
@@ -26,13 +26,13 @@ class HowItWorks extends Component {
                 </div>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="info info-horizontal">
-                <div class="icon icon-rose">
-                  <i class="material-icons">speaker_notes</i>
+            <div className="col-md-4">
+              <div className="info info-horizontal">
+                <div className="icon icon-rose">
+                  <i className="material-icons">speaker_notes</i>
                 </div>
-                <div class="description">
-                  <h4 class="info-title">2. Conte sua história</h4>
+                <div className="description">
+                  <h4 className="info-title">2. Conte sua história</h4>
                   <p>
                     Aqui a "moeda" é a sua história. Ao mostrar interesse em uma
                     doação, diga o porquê você deve ganhar esta doação..
@@ -40,13 +40,13 @@ class HowItWorks extends Component {
                 </div>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="info info-horizontal">
-                <div class="icon icon-rose">
-                  <i class="material-icons">commute</i>
+            <div className="col-md-4">
+              <div className="info info-horizontal">
+                <div className="icon icon-rose">
+                  <i className="material-icons">commute</i>
                 </div>
-                <div class="description">
-                  <h4 class="info-title">3. Combine a entrega</h4>
+                <div className="description">
+                  <h4 className="info-title">3. Combine a entrega</h4>
                   <p>
                     Ao ser escolhido, combine com o doador a melhor forma de
                     entrega para a sua doação.
