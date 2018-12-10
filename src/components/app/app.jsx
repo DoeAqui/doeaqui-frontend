ff1da07ed487df91ccf5ccc5d04b2b474ba5beb1
fernando <fernando.eho@gmail.com>
2018-12-09 22:00:10 -0200

    Adding register component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../home/home";
+import Register from "../user/register";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Switch>
+          <Route path="/register" component={Register} />
           <Route exact path="/" component={Home} />
         </Switch>
       </React.Fragment>
