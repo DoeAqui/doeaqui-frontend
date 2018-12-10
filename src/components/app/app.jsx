72f6c67cd292a2eccfca65554f226f79df177108
Fernando Oliveira <Fernando.Oliveira@enernoc.com>
2018-12-10 10:31:31 -0200

    Adding react-toastify

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Home from "../home/home";
 import Register from "../user/register";
 import AboutUs from "../about/about-us";
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <Switch>
           <Route path="/register" component={Register} />
           <Route path="/about" component={AboutUs} />
